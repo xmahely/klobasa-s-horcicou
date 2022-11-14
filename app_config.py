6b83302c00dde184092f3e945f727ff47bf17562
xmahely <xmahelyova@stuba.sk>
2022-11-14 16:39:37 +0100

    zadanie 3 done

--- app_config.py
+++ app_config.py
@@ -2,6 +2,8 @@ from datetime import timedelta
 from flask import Flask
 from flask_login import LoginManager, UserMixin
 import fldr
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 from flask_sqlalchemy import SQLAlchemy
 import encrypt_decrypt as ED
 
@@ -13,10 +15,16 @@ app.config['UPLOAD_FOLDER'] = fldr.UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024 * 1024
 app.secret_key = 'U1a2C&5f#kfu#8kU8W5A'
 app.permanent_session_lifetime = timedelta(minutes=30)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://db_user:ghpeworvaozzks@localhost/upb'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://db_user:ghpeworvaozzks@localhost/upb'
 # todo: pri pridávani na server odkomentovať, upb musí byť lowercase!
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://dbadmin:ghpeworvaozzks@localhost/upb'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://dbadmin:ghpeworvaozzks@localhost/upb'
 db = SQLAlchemy(app)
+# limiter = Limiter(
+#     app,
+#     key_func=get_remote_address,
+#     storage_uri="memcached://localhost:50001",
+#     storage_options={}
+# )
 
 
 class User(db.Model, UserMixin):
@@ -26,18 +34,18 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(255))
     psswd = db.Column(db.String(255))
     salt = db.Column(db.String(255))
-    pub = db.Column(db.String(255), default=None)
-    priv = db.Column(db.String(255), default=None)
+    pub = db.Column(db.String(511), default=None)
+    priv = db.Column(db.String(2047), default=None)
     is_active = db.Column(db.Boolean, default=True)
     authenticated = db.Column(db.Boolean, default=False)
 
-    def __init__(self, name, email, psswd, salt):
+    def __init__(self, name, email, psswd, salt, private, public):
         self.name = name
         self.email = email
         self.psswd = psswd
         self.salt = salt
-        self.pub = None
-        self.priv = None
+        self.pub = public
+        self.priv = private
 
     def change_keys(self, private, public):
         self.priv = private.encode()
@@ -55,3 +63,79 @@ class User(db.Model, UserMixin):
     def is_anonymous(self):
         """False, as anonymous users aren't supported."""
         return False
+
+    def getUserNameById(id):
+        return User.query.get(id).name
+
+    def getIdByUserName(name):
+        try:
+            return User.query.filter_by(name=name).first().user_id
+        except:
+            return None
+
+    def getIdByEmail(email):
+        try:
+            return User.query.filter_by(email=email).first()
+        except:
+            return None
+
+    def getPublicKey(id):
+        return User.query.filter_by(user_id=id).first().pub
+
+    def getPrivateKey(id):
+        return User.query.filter_by(user_id=id).first().priv
+
+    def getUserbyId(id):
+        try:
+            return User.query.filter_by(user_id=id).first()
+        except:
+            return None
+
+    @login.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
+
+    def __init__(self, name, email, psswd, salt, private, public):
+        self.name = name
+        self.email = email
+        self.psswd = psswd
+        self.salt = salt
+        self.pub = public
+        self.priv = private
+    
+class Message(db.Model):
+    message_id = db.Column('message_id', db.Integer, primary_key=True)
+    sender_ID = db.Column(db.Integer)
+    recipient_ID = db.Column(db.Integer)
+    messageLocation = db.Column(db.String(255))
+
+
+    @staticmethod
+    def create(sender_ID, recipient_ID, messageLocation):  # create new user
+        new_message = Message(sender_ID, recipient_ID, messageLocation)
+        db.session.add(new_message)
+        db.session.commit()
+
+
+    def get_id(self):
+        return self.user_id
+
+
+
+    def __init__(self, sender_ID, recipient_ID, messageLocation):
+        self.sender_ID = sender_ID
+        self.recipient_ID = recipient_ID
+        self.messageLocation = messageLocation
+
+
+def selectMessages(user_ID):
+    #s = select(Message.recipient_ID).where(Message.sender_ID == user_ID)
+    s = Message.query.filter((Message.sender_ID==user_ID) |(Message.recipient_ID==user_ID)).all()
+    #s += Message.query.filter_by(recipient_ID=1).all()
+    
+    return s
+
+
+with app.app_context():
+    db.create_all()
--- flaskapp.py
+++ flaskapp.py
@@ -5,7 +5,11 @@ from flask_login import current_user, login_user, logout_user, login_required
 import fldr
 import data_handler
 import encrypt_decrypt as ED
-from app_config import app, db, login, User
+from app_config import app, db, login, User, Message, selectMessages
+import uuid
+from datetime import datetime, timedelta
+from pytz import utc
+import json
 
 with app.app_context():
     db.create_all()
@@ -22,6 +26,7 @@ def home():
 
 
 @app.route("/register", methods=["POST", "GET"])
+# @limiter.limit("1/second") TODO toto by malo zabranit ddos akurat to nejde uplne dobre
 def register():
     if request.method == "POST":
         username = request.form["username"].strip()
@@ -29,58 +34,81 @@ def register():
         password = request.form["password"]
         confirm = request.form["password2"]
         # vytváranie nového užívateľa sa bude volať tu, nie vnútri classy User
-        psswd, salt = ED.psswd_hash(password)
-        new_user = User(username, email, psswd, salt)
-        db.session.add(new_user)
-        try:
-            db.session.commit()
-        except Exception as e:
-            return "error" + str(e)
-        return "ok"
-
-        # existing_user_username = User.query.filter_by
-        # (name=username).first()  # najde prvu zhodu v DB
-        # existing_email = User.query.filter_by(email=email).first()
-        # check = ED.psswd_check(password, confirm)
-        # if existing_user_username:
-        #     flash('This username already exists, try a new one!')
-        # elif existing_email:
-        #     flash('This email address is already in use!')
-        # elif check == 1:
-        #     flash('Password too weak')
-        # elif check == -1:
-        #     flash('Passwords do not match')
-        # else:
-        #     User.create(username, email, password)
-        #     return redirect(url_for("login"))
+        existing_user_username = User.getIdByUserName(username)  # najde prvu zhodu v DB
+        existing_email = User.getIdByEmail(email)
+        check = ED.psswd_check(password, confirm)
+        if existing_user_username:
+            flash('This username already exists, try a new one!')
+        elif existing_email:
+            flash('This email address is already in use!')
+        elif check == 1:
+            flash('Password too weak')
+        elif check == -1:
+            flash('Passwords do not match')
+        else:
+            psswd, salt = ED.psswd_hash(password)
+            private, public = ED.generate_rsa_pair()
+            new_user = User(username, email, psswd, salt, private.encode(), public)
+            db.session.add(new_user)
+            try:
+                db.session.commit()
+                path = "/home/Users/" + username
+                os.mkdir(path)
+                make_key_files(username,private,public)
+                # TODO toto nech neni warning
+                # namalo by sa stat ze existuje uz folder kedze username je unique
+                flash('Registration successful','')
+                return render_template('register.html')
+            except Exception as e:
+                return render_template("register.html")
     return render_template("register.html")
-
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
     if request.method == "POST":
-        if 'timeout' not in session:  # cas medzi prihlaseniami
-            session['timeout'] = 0
+        if 'timeout' in session:
+            if session.get('timeout').replace(tzinfo=utc) > datetime.now().replace(tzinfo=utc):
+                flash(f"You are locked out. You can log in after "
+                      f"{session.get('timeout').strftime('%d.%m.%Y %H:%M:%S')}.")
+                return redirect(request.url)
+        if 'timeout_count' not in session:
+            session['timeout_count'] = 1
         if 'counter' not in session:
             session['counter'] = 0
         session.permanent = True
         user = request.form["username"].strip()
         password = request.form["password"]
         u = User.query.filter_by(name=user).first()
-        # TODO odpocitavanie kedy povoli dalsi pokus o prihlasenie
         try:
             if ED.authenticate(u.salt, password.strip(), u.psswd) == 0:
+                if 'timeout' in session:
+                    timeout = session.get('timeout')
+                    session.pop('timeout', None)
+                    if timeout.replace(tzinfo=utc) > datetime.now().replace(tzinfo=utc):
+                        flash(f"You are locked out. You can log in after "
+                              f"{session.get('timeout').strftime('%d.%m.%Y %H:%M:%S')}.")
+                        return redirect(request.url)
                 login_user(u)
+                session.pop('counter', None)
+                session.pop('timeout_count', None)
                 return render_template("index.html")
             else:
+                flash(f"The username or password is incorrect!")
                 if 'counter' in session:
                     session['counter'] = session.get('counter') + 1
                 if session.get('counter') > 3:
-                    session.pop('counter', None)
-                    session['timeout'] = session.get('timeout') + 10
-                return render_template("login.html")
-        except Exception:  # neexistuje uzivatel
-            return render_template("login.html")
+                    if 'timeout' not in session:
+                        session['timeout'] = datetime.now().replace(tzinfo=utc)
+                    session['timeout_count'] = session.get('timeout_count') * 6
+                    if 'timeout' not in session:
+                        session['timeout'] = 0
+                    session['timeout'] = session.get('timeout') + timedelta(seconds=session.get('timeout_count'))
+                    flash(f"You have tried to log in {session.get('counter')} times. You can log in after "
+                          f"{session.get('timeout').strftime('%d.%m.%Y %H:%M:%S')}.")
+                return redirect(request.url)
+        except Exception:
+            flash(f"The username does not exist!")
+            return redirect(request.url)
     else:
         if current_user.is_authenticated:
             return redirect(url_for("user"))
@@ -97,7 +125,9 @@ def logout():
 def user():
     if current_user.is_authenticated:
         return render_template("user_page.html", username=current_user.name, email=current_user.email,
-                               p_key=current_user.pub)
+                               p_key=current_user.pub,
+                               path_public_key = fldr.UPLOAD_FOLDER + current_user.name + "/id_rsa_public.pub",
+                               path_private_key = fldr.UPLOAD_FOLDER + current_user.name + "/id_rsa.pem" )
     else:
         return redirect(url_for("login"))
 
@@ -111,8 +141,8 @@ def allowed_file(file, extension):
 @app.route("/download/<path:path>")
 def download_file(path):
     # todo: pri pridávani na server odkomentovať, musí tam byť /
-    #return send_file("/"+path, as_attachment=True)
-    return send_file(path, as_attachment=True)
+    return send_file("/"+path, as_attachment=True)
+    #return send_file(path, as_attachment=True)
 
 
 @app.route("/download/documentation")
@@ -217,27 +247,35 @@ def decrypt():
         return render_template("decrypt.html")
 
 
+def make_key_files(username, private_key, public_key):
+    path_public_key = os.path.join(fldr.UPLOAD_FOLDER, username, 'id_rsa_public.pub')
+    path_private_key = os.path.join(fldr.UPLOAD_FOLDER, username, 'id_rsa.pem')
+    f = open(path_private_key, "w+")
+    f.write(private_key)
+    f.close()
+    f = open(path_public_key, "w+")
+    f.write(public_key)
+    f.close()
+
+
 @app.route("/generate", methods=["POST", "GET"])
 @login_required
 def generate_keys():
     if request.method == "POST":
         private_key, public_key = ED.generate_rsa_pair()
-        path_public_key = os.path.join(fldr.UPLOAD_FOLDER, 'public_key.pub')
-        path_private_key = os.path.join(fldr.UPLOAD_FOLDER, 'private_key.pem')
-        f = open(path_private_key, "w+")
-        f.write(private_key)
-        f.close()
-        f = open(path_public_key, "w+")
-        f.write(public_key)
-        f.close()
-
-        u = User.query.filter_by(name=current_user.name).first()
-        u.pub = public_key
-        u.priv = private_key.encode()
-        db.session.commit()
-
-        return render_template('download2.html', header="Generated keys", path_public_key=path_public_key,
-                               path_private_key=path_private_key)
+        make_key_files(current_user.name, private_key,public_key)
+        try:
+            u = User.getUserbyId(current_user.user_id)
+            if u is None:
+                raise Exception()
+            u.priv = private_key.encode()
+            u.pub = public_key
+            db.session.commit()
+            return render_template('download2.html', header="Generated keys", path_public_key=path_public_key,
+                                   path_private_key=path_private_key)
+        except Exception:
+            flash("Unexpected error")
+            return render_template("generate.html")
     else:
         return render_template("generate.html")
 
@@ -247,8 +285,103 @@ def nope():
     return "I give up :("
 
 
+#<input class="textarea form-control" type="textarea" id="text" name="text" rows="4">
+@app.route("/chat/", methods=["POST", "GET"],defaults={'chatterID': 0})
+@app.route("/chat/<int:chatterID>", methods=["POST", "GET"])
+@login_required
+def chat(chatterID = 0):
+    #print(chatterID)
+    if request.method == "POST":
+        
+        message = request.form.get('message', "error")
+        newUser = request.form.get('newUser', chatterID)
+        if (newUser!=chatterID and (User.getIdByUserName(newUser) == None)) or (User.getIdByUserName(newUser) == current_user.user_id):
+            json =getJSON(current_user.user_id)
+            return render_template("chat.html", messages=json, displayUser=chatterID)
+        if (message == "error" and newUser != chatterID):
+            message = "Hello"
+            newUser = User.getIdByUserName(newUser)
+        #Moving forward code
+        #print("TO: ",newUser)
+        #print("FROM: ",current_user.user_id)
+
+        public_key_path = ("tmp/public_key.pub")
+
+            
+
+        text_file_string = message
+
+        
+        public_key_string = data_handler.read_public_key3(User.getPublicKey(newUser))
+        #print(public_key_string)
+        if not public_key_string:
+            flash('Key has to have correct format !')
+            return redirect(request.url)
+        else:
+            encrypted_text_string = ED.encrypt_file(text_file_string, public_key_string)
+            unique_filename = str(uuid.uuid4())
+            path = os.path.join(fldr.UPLOAD_FOLDER,'messages', unique_filename)
+            f = open(path, "w+")
+            f.write(encrypted_text_string)
+            f.close()
+            
+
+
+        
+        Message.create(current_user.user_id, newUser, path)
+        json =getJSON(current_user.user_id)
+        #print(selectMessages(1))
+        return render_template("chat.html", messages=json, displayUser=chatterID)
+    else:
+
+
+        
+        json =getJSON(current_user.user_id)
+        #print(selectMessages(1))
+        return render_template("chat.html", messages=json, displayUser=chatterID)
+
+def getJSON(user_ID):
+    struct = {}
+    try:
+        messages = selectMessages(user_ID)
+        #print(messages)
+        for message in messages:
+            
+            #private_key_path = ("tmp/private_key.pem")
+            #private_key_string = data_handler.read_private_key2(private_key_path)
+            private_key_string = data_handler.read_private_key3(User.getPrivateKey(message.recipient_ID))
+            
+            fp = open(message.messageLocation, 'r')
+            message_string = fp.read()
+            
+            fp.close() 
+            decrypted_text_string = ED.decrypt_file(message_string , private_key_string)
+            #print(message.recipient_ID)
+            if (message.sender_ID==user_ID):
+                #print(message.messageLocation)
+                struct.setdefault(message.recipient_ID,[]).append(tuple((User.getUserNameById(message.sender_ID), decrypted_text_string)))
+            if (message.recipient_ID==user_ID):
+                #print(message.messageLocation)
+                struct.setdefault(message.sender_ID,[]).append(tuple((User.getUserNameById(message.sender_ID), decrypted_text_string)))
+            
+        #print(struct)
+        #print(jsonData)
+        return struct
+    except:
+        print("OH MY LAWRD")
+        return struct
+@app.template_global()
+def getUserName(id):
+    return User.getUserNameById(id)
+
+# TODO deactivate user .. delete from db and delete system folder
+
+
 if __name__ == "__main__":
+    app.jinja_env.globals.update(getUserName=getUserName)
     with app.app_context():
         db.create_all()
+    
     app.debug = True
     app.run(host='0.0.0.0')
+    
